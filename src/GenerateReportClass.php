f7954d37a8856ef57cdbcc1a5cd5dbe89a95903d
Shital <s.adhegaonkar@easternenterprise.com>
2017-01-30 15:26:27 +0530

    updated GenerateReportClass

--- src/GenerateReportClass.php
+++ src/GenerateReportClass.php
@@ -39,12 +39,11 @@ class GenerateReportClass
 
 		//generate Codesniffer report
 		$codesnifferReport = 'reports/codesniffer/phpcs.csv';
-		chmod('vendor/bin/', 0777); 
-		exec('vendor/bin/phpcs --standard=reports/phprcs.xml app > '.$codesnifferReport);	
+		exec('php vendor/bin/phpcs --standard=reports/phprcs.xml app > '.$codesnifferReport);	
 
 		//generate Mess detector report
 		$messDetectorReport = 'reports/phpmd/phpmd.csv';		
-		exec('vendor/bin/phpmd app text reports/phprmd.xml > '.$messDetectorReport);	    		
+		exec('php vendor/bin/phpmd app text reports/phprmd.xml > '.$messDetectorReport);	    		
 
 		//convert txt report file to excel
 		header('Content-type: application/ms-excel');
