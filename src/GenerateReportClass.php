768a26e39020ed03704249149603cec836dde4a7
Shital <s.adhegaonkar@easternenterprise.com>
2017-02-01 12:20:42 +0530

    changes done

--- src/GenerateReportClass.php
+++ src/GenerateReportClass.php
@@ -48,9 +48,9 @@ class GenerateReportClass
 		return true;
     }
 
-    public static function convertReportToExcel($csv_file, $xls_file)
+    public static function convertReportToExcel($csv_file, $xls_file,$csv_enc = null)
     {
-        $objReader = PHPExcel_IOFactory::createReader('CSV');
+        /*$objReader = PHPExcel_IOFactory::createReader('CSV');
 
         // If the files uses a delimiter other than a comma (e.g. a tab), then tell the reader
         $objReader->setDelimiter("\t");
@@ -60,7 +60,44 @@ class GenerateReportClass
 
         $objPHPExcel = $objReader->load($csv_file);
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-        $objWriter->save($xls_file.'.xls');
+        $objWriter->save($xls_file.'.xls');*/
+
+        //convert csv report file to excel
+        header('Content-type: application/ms-excel');
+        header('Content-Disposition: attachment; filename='.'test.xlsx');
+        //set cache
+        $cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_phpTemp;
+        PHPExcel_Settings::setCacheStorageMethod($cacheMethod);
+
+        //open csv file
+        $objReader = new PHPExcel_Reader_CSV();
+        if ($csv_enc != null)
+            $objReader->setInputEncoding($csv_enc);
+        $objPHPExcel = $objReader->load($csv_file);
+        $in_sheet = $objPHPExcel->getActiveSheet();
+
+        //open excel file
+        $objPHPExcel = new PHPExcel();
+        $out_sheet = $objPHPExcel->getActiveSheet();
+
+        //row index start from 1
+        $row_index = 0;
+        foreach ($in_sheet->getRowIterator() as $row) {
+            $row_index++;
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false);
+
+            //column index start from 0
+            $column_index = -1;
+            foreach ($cellIterator as $cell) {
+                $column_index++;
+                $out_sheet->setCellValueByColumnAndRow($column_index, $row_index, $cell->getValue());
+            }
+        }
+
+        //write excel file
+        $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+        $objWriter->save($xls_file.'.xlsx');
     	
         return true;
     }
