87e1620c63fafdd52af8a3235fa8b5e0f1916961
Shital <s.adhegaonkar@easternenterprise.com>
2017-02-01 10:34:45 +0530

    clean code

--- src/GenerateReportClass.php
+++ src/GenerateReportClass.php
@@ -62,45 +62,7 @@ class GenerateReportClass
         $objPHPExcel = $objReader->load('MyCSVFile.csv');
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
         $objWriter->save('MyExcelFile.xls');
-    	/*ob_start(); 	
-
-    	//convert csv report file to excel
-		header('Content-type: application/ms-excel');
-		header('Content-Disposition: attachment; filename='.'test.xlsx');
-        //set cache
-        $cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_phpTemp;
-        PHPExcel_Settings::setCacheStorageMethod($cacheMethod);
-
-        //open csv file
-        $objReader = new PHPExcel_Reader_CSV();
-        if ($csv_enc != null)
-            $objReader->setInputEncoding($csv_enc);
-        $objPHPExcel = $objReader->load($csv_file);
-        $in_sheet = $objPHPExcel->getActiveSheet();
-
-        //open excel file
-        $objPHPExcel = new PHPExcel();
-        $out_sheet = $objPHPExcel->getActiveSheet();
-
-        //row index start from 1
-        $row_index = 0;
-        foreach ($in_sheet->getRowIterator() as $row) {
-            $row_index++;
-            $cellIterator = $row->getCellIterator();
-            $cellIterator->setIterateOnlyExistingCells(false);
-
-            //column index start from 0
-            $column_index = -1;
-            foreach ($cellIterator as $cell) {
-                $column_index++;
-                $out_sheet->setCellValueByColumnAndRow($column_index, $row_index, $cell->getValue());
-            }
-        }
-
-        //write excel file
-        $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
-        $objWriter->save($xls_file);        
-		ob_end_flush();*/
+    	
         return true;
     }
 }
