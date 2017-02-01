e72647e68115916bb626a4e794cf1c70e7e25114
Shital <s.adhegaonkar@easternenterprise.com>
2017-02-01 12:33:27 +0530

    changes done

--- src/GenerateReportClass.php
+++ src/GenerateReportClass.php
@@ -11,7 +11,6 @@ class GenerateReportClass
     public static function generateReport(Event $event)
     {    	
         $composer = $event->getComposer();
-        $event->getIO()->write("Show me after INSTALL command");
         if (!file_exists('reports')) {
     		mkdir('reports', 0777, true);  
     		self::createDir();
